8bb2a9d2103a535bb85d9ee374b13e4e5b17dcce
pranjaltrav <kkant@traversia.net>
2025-05-09 11:25:42 +0530

    dealer master

--- TSCDB.Core/Domain/Dealer/BorrowerDetails.cs
+++ TSCDB.Core/Domain/Dealer/BorrowerDetails.cs
@@ -4,19 +4,26 @@
     {
         public int Id { get; set; }
         public int DealerId { get; set; }
+        public Dealer Dealer { get; set; }
+
+        public string PersonType { get; set; } // New
         public string Name { get; set; }
         public string PAN { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string FatherName { get; set; }
+        public string AadharNo { get; set; } // New
+        public DateTime? DateOfBirth { get; set; }
         public string MobileNumber { get; set; }
         public string Email { get; set; }
-        public string CIBILScore { get; set; }
+
+        public string FatherName { get; set; }
         public string RelationshipWithEntity { get; set; }
+
         public string CurrentAddress { get; set; }
         public string PermanentAddress { get; set; }
-        public string AttachmentPath { get; set; }
 
-        public virtual Dealer Dealer { get; set; }
-        public virtual ICollection<DocumentUpload> DocumentUploads { get; set; }
+        public string AddressStatus { get; set; } // New
+        public DateTime? AddressAgreementDate { get; set; } // New
+        public DateTime? AddressAgreementExpiryDate { get; set; } // New
+
+        public int? CIBILScore { get; set; }
     }
 }
--- TSCDB.Core/Domain/Dealer/Dealer.cs
+++ TSCDB.Core/Domain/Dealer/Dealer.cs
@@ -11,37 +11,64 @@ namespace TscLoanManagement.TSCDB.Core.Domain.Dealer
         public string LoanProposalNo { get; set; }
         public string DealershipName { get; set; }
         public string DealershipPAN { get; set; }
-        //public string Status { get; set; }
-        public DateTime DateOfIncorporation { get; set; }
-        public string Entity { get; set; }
         public string GSTNo { get; set; }
+        public string GSTRegStatus { get; set; }
         public string MSMERegistrationNo { get; set; }
-        public string MSMEType { get; set; } // Medium, Small, Micro
-
+        public string MSMEType { get; set; }
+        public string MSMEStatus { get; set; }
+        public string BusinessCategory { get; set; }
+        public string BusinessType { get; set; }
+        public string Entity { get; set; } // TypeOfEntity
         public string ContactNo { get; set; }
         public string AlternativeContactNo { get; set; }
         public string EmailId { get; set; }
         public string AlternativeEmailId { get; set; }
+
         public string ShopAddress { get; set; }
         public string ParkingYardAddress { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string State { get; set; }
+        public string Pincode { get; set; }
+
+        public string OfficeStatus { get; set; }
+        public DateTime? AgreementDate { get; set; }
+        public DateTime? AgreementExpiryDate { get; set; }
+
+        public string ParkingStatus { get; set; }
+        public DateTime? ParkingAgreementDate { get; set; }
+        public DateTime? ParkingAgreementExpiryDate { get; set; }
+
+        public DateTime? DateOfIncorporation { get; set; }
+        public DateTime? DateOfFacilityAgreement { get; set; }
+        public string CIBILOfEntity { get; set; }
+
+        public decimal? TotalSanctionLimit { get; set; }
+        public decimal? ROI { get; set; }
+        public decimal? ROIPerLakh { get; set; }
+        public decimal? DelayROI { get; set; }
+
+        public decimal? ProcessingFee { get; set; }
+        public decimal? ProcessingCharge { get; set; }
+        public decimal? GSTOnProcessingCharge { get; set; }
+        public decimal? DocumentationCharge { get; set; }
+        public decimal? GSTOnDocCharges { get; set; }
 
         public string RelationshipManager { get; set; }
+        public string DealerStatus { get; set; }
+        public string RejectionReason { get; set; }
 
         public bool IsActive { get; set; }
-        public DateTime RegisteredDate { get; set; }
-        public int UserId { get; set; }
-        public virtual User User { get; set; }
-        public virtual ICollection<TscLoanManagement.TSCDB.Core.Domain.Loan.Loan> Loans { get; set; }
 
-        public ICollection<ChequeDetails> ChequeDetails { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public virtual ICollection<TscLoanManagement.TSCDB.Core.Domain.Loan.Loan> Loans { get; set; }
         public ICollection<BorrowerDetails> BorrowerDetails { get; set; }
         public ICollection<GuarantorDetails> GuarantorDetails { get; set; }
+        public ICollection<ChequeDetails> ChequeDetails { get; set; }
         public ICollection<SecurityDepositDetails> SecurityDepositDetails { get; set; }
-
-        public virtual ICollection<DocumentUpload> DocumentUploads { get; set; }
-
-        public DealerStatus Status { get; set; }
-        public string? RejectionReason { get; set; } // Nullable
+        public ICollection<DocumentUpload> DocumentUploads { get; set; }
 
 
     }
